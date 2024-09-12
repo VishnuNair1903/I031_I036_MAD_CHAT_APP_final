a7a9c4206e4dee22141dad77a0954efaa232a493
VishnuNair1903 <vishn1903@gmail.com>
2024-09-12 23:12:49 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://www.jitpack.io") } // Fixed the assignment operator '=' instead of ':'
+    }
+}
+
+
+rootProject.name = "I031_I036_MAD_CHAT_APP"
+include(":app")
+ 
